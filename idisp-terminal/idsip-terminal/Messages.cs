c43470360e8736e4a215e1807c924f99a7d80e12
Albert Kiefel - SKIDATA <kial@skidata.net>
2018-12-10 14:45:11 +0100

    inital commit

---
+++ idisp-terminal/idsip-terminal/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace idsip_terminal
+{
+    class Messages
+    {
+        public string Command { get; set; }
+        public object Args { get; set; }
+    }
+
+    class Transaction
+    {
+        public string TransactionID { get; set; }
+        public string LP { get; set; }
+        public DateTime Time { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Payment
+    {
+        public double Price { get; set; }
+    }
+}
---
+++ idisp-terminal/idsip-terminal/Program.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace idsip_terminal
+{
+    public class Laputa : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+            Send(JsonConvert.SerializeObject(new Messages
+            {
+                Command = "View",
+                Args = "idle"
+            }));
+
+            Send(JsonConvert.SerializeObject(new Messages
+            {
+                Command = "View",
+                Args = "transaction"
+            }));
+
+            Send(JsonConvert.SerializeObject(new Messages
+            {
+                Command = "Transaction",
+                Args = new Transaction
+                {
+                    TransactionID = "1234567",
+                    LP = "BGL K 714",
+                    Price = 5.0,
+                    Time = DateTime.Now
+                }
+            }));
+
+            Send(JsonConvert.SerializeObject(new Messages
+            {
+                Command = "View",
+                Args = "payment"
+            }));
+
+            Send(JsonConvert.SerializeObject(new Messages
+            {
+                Command = "Payment",
+                Args = new Payment
+                {
+                    Price = 5.0
+                }
+            }));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://localhost:8080");
+            wssv.AddWebSocketService<Laputa>("/service");
+            wssv.Start();
+
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+ }
